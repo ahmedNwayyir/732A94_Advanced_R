e7e0e8af69a81943f7633d267dfbe087ff32203b
Ahmed Nwayyir <nwayyir.ahmed@gmail.com>
2020-08-28 11:31:05 +0200

    re-arranged files

--- Exams/2018/201810223.R
+++ Exams/2018/201810223.R
@@ -1,76 +1,83 @@
 # Problem 2
 batory <- setRefClass("batory", 
-                            fields = list(fuel = "numeric",
-                                          max_cargo_weight = "numeric",
-                                          cargo_weight = "numeric",
-                                          entity = "data.frame"),
-                          methods = list(
-                            build_Batory = function(fuel, max_cargo_weight){
-                              fuel <<- fuel
-                              max_cargo_weight <<- max_cargo_weight
-                              if(fuel > max_cargo_weight){
-                                return("The fuel sould not greater than max_cargo_weight!")
-                              }
-                              cargo_weight <<- fuel
-                              entity <<- data.frame("name" = "fuel", "weight" = fuel, stringsAsFactors = FALSE )
-                              return(list("entity" = entity, "cargo_weight" = cargo_weight))
-                            },
-                            equip_Batory = function(name, equip_weight){
-                              stopifnot(is.character(name) && is.numeric(equip_weight))
-                              if(equip_weight + cargo_weight > max_cargo_weight){
-                                return("This object is too heavy!")
-                              }else{
-                                new_entity = c(name, equip_weight)
-                                entity <<- rbind(entity, new_entity)
-                                cargo_weight <<- cargo_weight + equip_weight
-                                
-                                return(list("entity" = entity, "cargo_weight" = cargo_weight))
-                              }
-                            },
-                            embark_sailor = function(sailor_weight){
-                              if(sailor_weight > max_cargo_weight - fuel){
-                                return("This sailor is too heavy!")
-                              }else{
-                                i <- 1
-                                list <- c()
-                                while(sailor_weight + cargo_weight > max_cargo_weight){
-                                  if(i > nrow(entity)){
-                                    return("Sorry, this sailor can not embark.")
-                                  }
-                                  if(entity$name[i] != "fuel" && entity$name[i] != "sailor"){
-                                    list <- c(list, i)
-                                    cargo_weight <<- cargo_weight - as.numeric(entity$weight[i])
-                                  }
-                                  i <- i + 1
-                                }
-                                if(length(list) != 0){
-                                  entity <<- entity[-list,]
-                                }
-                                equip_Batory("sailor", sailor_weight)
-                                return(list("entity" = entity, "cargo_weight" = cargo_weight))
-                              }
-                            },
-                            print = function(){
-                              print_result <- list(max_cargo_weight, cargo_weight, fuel, entity)
-                              names(print_result) <- c("max_cargo_weight", "cargo_weight", "fuel", "entity")
-                              return(print_result)
-                            },
-                            plot = function(){
-                              entity_names <- unique(entity$name)
-                              weight_list <- vector()
-                              for(i in entity_names){
-                                total_weight <- sum(as.numeric(entity[which(entity$name == i),]$weight))
-                                weight_list <- c(weight_list, total_weight)
-                              }
-                              barplot(weight_list, 
-                                      names.arg = entity_names, 
-                                      xlab = "Entity Names",
-                                      ylab = "Total Weight",
-                                      col = "blue",
-                                      main = "Entity chart",
-                                      border = "red")
-                            }
-                          ))
+  
+  fields = list(fuel = "numeric",
+                  max_cargo_weight = "numeric",
+                  cargo_weight = "numeric",
+                  entity = "data.frame"),
+    
+  methods = list(
+    
+    build_Batory = function(fuel, max_cargo_weight){
+      fuel <<- fuel
+      max_cargo_weight <<- max_cargo_weight
+      if(fuel > max_cargo_weight){
+        return("The fuel sould not greater than max_cargo_weight!")
+      }
+      cargo_weight <<- fuel
+      entity <<- data.frame("name" = "fuel", "weight" = fuel, stringsAsFactors = FALSE )
+      return(list("entity" = entity, "cargo_weight" = cargo_weight))
+    },
+    
+    equip_Batory = function(name, equip_weight){
+      stopifnot(is.character(name) && is.numeric(equip_weight))
+      if(equip_weight + cargo_weight > max_cargo_weight){
+        return("This object is too heavy!")
+      }else{
+        new_entity = c(name, equip_weight)
+        entity <<- rbind(entity, new_entity)
+        cargo_weight <<- cargo_weight + equip_weight
+        
+        return(list("entity" = entity, "cargo_weight" = cargo_weight))
+      }
+    },
+    
+    embark_sailor = function(sailor_weight){
+      if(sailor_weight > max_cargo_weight - fuel){
+        return("This sailor is too heavy!")
+      }else{
+        i <- 1
+        list <- c()
+        while(sailor_weight + cargo_weight > max_cargo_weight){
+          if(i > nrow(entity)){
+            return("Sorry, this sailor can not embark.")
+          }
+          if(entity$name[i] != "fuel" && entity$name[i] != "sailor"){
+            list <- c(list, i)
+            cargo_weight <<- cargo_weight - as.numeric(entity$weight[i])
+          }
+          i <- i + 1
+        }
+        if(length(list) != 0){
+          entity <<- entity[-list,]
+        }
+        equip_Batory("sailor", sailor_weight)
+        return(list("entity" = entity, "cargo_weight" = cargo_weight))
+      }
+    },
+    
+    print = function(){
+      print_result <- list(max_cargo_weight, cargo_weight, fuel, entity)
+      names(print_result) <- c("max_cargo_weight", "cargo_weight", "fuel", "entity")
+      return(print_result)
+    },
+    
+    plot = function(){
+      entity_names <- unique(entity$name)
+      weight_list <- vector()
+      for(i in entity_names){
+        total_weight <- sum(as.numeric(entity[which(entity$name == i),]$weight))
+        weight_list <- c(weight_list, total_weight)
+      }
+      barplot(weight_list, 
+              names.arg = entity_names, 
+              xlab = "Entity Names",
+              ylab = "Total Weight",
+              col = "blue",
+              main = "Entity chart",
+              border = "red")
+    }
+  ))
 
 ORP_Batory <- batory$new()
 ORP_Batory$build_Batory(fuel=1.5,max_cargo_weight=10)
@@ -101,17 +108,18 @@ multiplication <- function(A, B){
   return(V)
 }
 
-# A <- matrix(1:20, nrow = 5, byrow = TRUE)
-# B <- t(A)
-# multiplication(A, B)
-# print(A%*%B)
+A <- matrix(1:20, nrow = 5, byrow = TRUE)
+B <- t(A)
+multiplication(A, B)
+print(A%*%B)
 
-# b
-# A <- matrix(1:20, nrow = 5, byrow = TRUE)
-# B <- t(A)
-# test_that("The output is correct", {
-#   expect_that(multiplication(A, B), is_equivalent_to(A%*%B))
-# })
+#b
+library(testthat)
+A <- matrix(1:20, nrow = 5, byrow = TRUE)
+B <- t(A)
+test_that("The output is correct", {
+  expect_that(multiplication(A, B), is_equivalent_to(A%*%B))
+})
 
 
 
--- Exams/2018/20181210.R
+++ Exams/2018/20181210.R
@@ -1,81 +1,89 @@
 # Problem 2
 
 library <- setRefClass("ibrary",
-                       fields = list(book_capacity = "numeric",
-                                     number_of_books = "numeric",
-                                     number_of_loaned = "numeric",
-                                     books = "data.frame"),
-                       methods = list(
-                         build_library = function(book_capacity){
-                           stopifnot(book_capacity > 0)
-                           book_capacity <<- book_capacity
-                           number_of_books <<- 0
-                           number_of_loaned <<- 0
-                           books <<- data.frame(title = character(),
-                                                author = character(),
-                                                id = character(),
-                                                status = character(),
-                                                stringsAsFactors = FALSE)
-                           names(books) <<- c("title", "author", "id", "status")
-                           },
-                         acquire_book = function(title, author){
-                           if(number_of_books == book_capacity){
-                             return("This library is full, can not add book anymore.")
-                           }else{
-                            id <- sprintf("%05d", nrow(books)+1)
-                            new_book <- list("title" = title, "author" = author, "id" = id, "status" = "Available")
-                            books <<- rbind(books, new_book)
-                            number_of_books <<- number_of_books + 1
-                            result <- paste(title, "(", author,") have been added in library!")
-                             return(result)
-                           }
-                         },
-                         borrow_book = function(id){
-                           if(id %in% books$id){
-                             index <- which(books$id == id)
-                             if(books[index,]$status == "Available"){
-                               number_of_loaned <<- number_of_loaned + 1
-                               books[index,]$status <<- "Loaned"
-                               result <- "Borrow process succeed."
-                             }else{
-                               result <- "Is book is not avaliable now!" 
-                             }
-                             return(result)
-                           }else{
-                             return("Does not find this book, please check the ID.")
-                           }
-                         },
-                         return_book = function(id){
-                           if(id %in% books$id){
-                             index <- which(books$id == id)
-                             if(books[index,]$status == "Loaned"){
-                               number_of_loaned <<- number_of_loaned - 1
-                               books[index,]$status <<- "Available"
-                               result <- "Return process succeed."
-                             }else{
-                               result <- "Sorry, this book have not been borrowed." 
-                             }
-                             return(result)
-                           }else{
-                             return("Does not find this book, please check the ID.")
-                           }
-                         },
-                        print = function(){
-                          available_list <- books[which(books$status == "Available"),]
-                          loaned_list <- books[which(books$status == "Loaned"),]
-                          print_list <- list(nrow(available_list), available_list, nrow(loaned_list), loaned_list)
-                          names(print_list) <- c("Number of Available", "Available", "Number of Loaned", "Loaned")
-                          return(print_list)
-                        },
-                        plot = function(){
-                          status_list <- c(length(which(books$status == "Available")), length(which(books$status == "Loaned")))
-                          barplot(status_list,
-                                  names.arg = c("Available", "Loaned"),
-                                  xlab = "Book Status",
-                                  ylab = "Number",
-                                  main = "Book Possession")
-                        }
-                         ))
+                       
+   fields = list(book_capacity = "numeric",
+                 number_of_books = "numeric",
+                 number_of_loaned = "numeric",
+                 books = "data.frame"),
+   
+   methods = list(
+     
+     build_library = function(book_capacity){
+       stopifnot(book_capacity > 0)
+       book_capacity <<- book_capacity
+       number_of_books <<- 0
+       number_of_loaned <<- 0
+       books <<- data.frame(title = character(),
+                            author = character(),
+                            id = character(),
+                            status = character(),
+                            stringsAsFactors = FALSE)
+       names(books) <<- c("title", "author", "id", "status")
+       },
+     
+     acquire_book = function(title, author){
+       if(number_of_books == book_capacity){
+         return("This library is full, can not add book anymore.")
+       }else{
+        id <- sprintf("%05d", nrow(books)+1)
+        new_book <- list("title" = title, "author" = author, "id" = id, "status" = "Available")
+        books <<- rbind(books, new_book)
+        number_of_books <<- number_of_books + 1
+        result <- paste(title, "(", author,") have been added in library!")
+         return(result)
+       }
+     },
+     
+     borrow_book = function(id){
+       if(id %in% books$id){
+         index <- which(books$id == id)
+         if(books[index,]$status == "Available"){
+           number_of_loaned <<- number_of_loaned + 1
+           books[index,]$status <<- "Loaned"
+           result <- "Borrow process succeed."
+         }else{
+           result <- "Is book is not avaliable now!" 
+         }
+         return(result)
+       }else{
+         return("Does not find this book, please check the ID.")
+       }
+     },
+     
+     return_book = function(id){
+       if(id %in% books$id){
+         index <- which(books$id == id)
+         if(books[index,]$status == "Loaned"){
+           number_of_loaned <<- number_of_loaned - 1
+           books[index,]$status <<- "Available"
+           result <- "Return process succeed."
+         }else{
+           result <- "Sorry, this book have not been borrowed." 
+         }
+         return(result)
+       }else{
+         return("Does not find this book, please check the ID.")
+       }
+     },
+     
+    print = function(){
+      available_list <- books[which(books$status == "Available"),]
+      loaned_list <- books[which(books$status == "Loaned"),]
+      print_list <- list(nrow(available_list), available_list, nrow(loaned_list), loaned_list)
+      names(print_list) <- c("Number of Available", "Available", "Number of Loaned", "Loaned")
+      return(print_list)
+    },
+    
+    plot = function(){
+      status_list <- c(length(which(books$status == "Available")), length(which(books$status == "Loaned")))
+      barplot(status_list,
+              names.arg = c("Available", "Loaned"),
+              xlab = "Book Status",
+              ylab = "Number",
+              main = "Book Possession")
+    }
+     ))
 
 my_library <- library$new()
 my_library$build_library(book_capacity = 100)
@@ -91,6 +99,9 @@ my_library$borrow_book("00002")
 
 my_library$print()
 my_library$plot()
+
+
+
 
 # Problem 3
 # a
